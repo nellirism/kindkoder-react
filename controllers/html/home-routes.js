a6c42e9d7c7f55dce8c351c48a0411fe85a38970
Cerebreo <nellirism@outlook.com>
2021-07-04 13:36:40 -0700

    round the corners of the project images

--- controllers/html/home-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// render home page
-router.get('/', (req, res) => {
-    res.render('homepage', {});
-});
-
-
-module.exports = router;
--- public/javascript/homepage.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('#book_appointment').addEventListener('click', function(event) {
-    window.location.replace('/');
-});
